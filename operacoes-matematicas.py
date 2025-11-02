62bf8a382774b644231b17825d7fc49de39adcff
Antonio Luiz Ferreira Neto <antonioluizfneto@gmail.com>
2025-11-02 15:14:19 -0300

    Add files via upload
    Treinando e Utilizando Operações de Matemática Básica em Python

---
+++ operacoes-matematicas.py
@@ -0,0 +1,3 @@
+n1=int(input('digite um numero:'))
+print('O sucessor de {} é {} e o antecessor é {}'.format(n1,n1+1,n1-1))
+print('Dobro: {} \nTriplo: {} \nRaiz Quadrada: {}'.format(n1*2,n1*3,n1**(1/2)))
